59f26b21e1143a8cfa90c82068120c2080ce7864
Utkarsh <hauntedutkarsh@gmail.com>
2023-09-26 07:30:51 +0530

    updated

---
+++ script.js
@@ -0,0 +1,42 @@
+var tl = gsap.timeline();
+
+gsap.set(".a", {
+  opacity: 0,
+  y: 10,
+});
+
+gsap.set("#right img", {
+  opacity: 0,
+  y: 10,
+  scale: 1.6,
+});
+
+tl.from("#left", {
+  width: 0,
+  duration: 1,
+  ease: Expo.easeInOut,
+})
+
+  .from("#right", {
+    width: 0,
+    duration: 1,
+    ease: Expo.easeInOut,
+  })
+
+  .to(".a", {
+    delay: -2,
+    stagger: .2,
+    opacity: 1,
+    y: 0,
+    ease: Expo.easeInOut,
+    duration: 1.5,
+  })
+
+  .to("#right img", {
+    delay: -1,
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    ease: Expo.easeInOut,
+    duration: 1,
+  });
